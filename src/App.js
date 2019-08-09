5d808b1661d43f5c14e6b63c52a29c188a53a68e
Vivekananda-Athukuri <10526003+Vivekananda-Athukuri@users.noreply.github.com>
2019-08-10 00:09:27 +0530

    Add titles

--- src/App.js
+++ src/App.js
@@ -8,28 +8,28 @@ function App() {
   return (
     <div className="App">
       <div className="App-main">
-        <div id="profile">
+        <div id="profile" title="Vivekananda Athukuri">
           <img src="/profile-2.jpg" alt="profile" />
         </div>
         <div className="name">
           <div id="firstname">Vivekananda</div>
           <div id="lastname">Athukuri</div>
         </div>
-        <div className="skype-wrapper">
+        <div className="skype-wrapper" title="Skype">
           <FontAwesomeIcon className="skype-icon" icon={faSkype} />
           <div className="skype-name">vivekananda.athukuri</div>
         </div>
         <div className="links">
-          <a className="App-link" id="github" href="https://github.com/Vivekananda-Athukuri" target="_blank" rel="noopener noreferrer">
+          <a className="App-link" id="github" href="https://github.com/Vivekananda-Athukuri" target="_blank" rel="noopener noreferrer" title="GitHub">
             <FontAwesomeIcon icon={faGithub} />
           </a>
-          <a className="App-link" id="linkedin" href="https://www.linkedin.com/in/vivekananda-athukuri/" target="_blank" rel="noopener noreferrer">
+          <a className="App-link" id="linkedin" href="https://www.linkedin.com/in/vivekananda-athukuri/" target="_blank" rel="noopener noreferrer" title="LinkedIn">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-          <a className="App-link" id="email" href="mailto:vivekananda.tagore@protonmail.com" target="_blank" rel="noopener noreferrer">
+          <a className="App-link" id="email" href="mailto:vivekananda.tagore@protonmail.com" target="_blank" rel="noopener noreferrer" title="Email">
             <FontAwesomeIcon icon={faEnvelope} />
           </a>
-          <a className="App-link" id="resume" href="https://www.visualcv.com/pdfs/5224336/" target="_blank" rel="noopener noreferrer">
+          <a className="App-link" id="resume" href="https://www.visualcv.com/pdfs/5224336/" target="_blank" rel="noopener noreferrer" title="Resume">
             <FontAwesomeIcon icon={faFilePdf} />
           </a>
         </div>
